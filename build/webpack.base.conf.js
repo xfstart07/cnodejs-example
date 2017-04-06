b51c7d43523c872fe1cb2c00e4ec4fbb5854b9a1
Leon Xu <xfstart07@gmail.com>
2017-04-06 09:48:05 +0800

    添加 topics

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,7 @@ var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
@@ -14,9 +14,8 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath: process.env.NODE_ENV === 'production' ?
+      config.build.assetsPublicPath : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
@@ -26,8 +25,7 @@ module.exports = {
     }
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
@@ -35,6 +33,14 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.scss$/,
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.css$/,
         include: [resolve('src'), resolve('test')]
       },
       {
--- src/components/topics.vue
+++ src/components/topics.vue
@@ -2,20 +2,69 @@
   <section id="page">
     <ul class="posts-list">
       <li v-for="item in topics" :key="item.id">
-        <router-link :to="{name: 'topic', params: {id: item.id}}"></router-link>
-        <h3 v-text="item.title" :class="getTabInfo(item.tab, item.good, item.top, ture)"
-          :title="getTabInfo(item.tab,item.good, item.top, false)"></h3>
+          <h3 v-text="item.title"></h3>
+          <div class="content">
+            <img :src="item.author.avatar_url" alt="Avatar">
+            <div class="info">
+              <p>
+                <span class="name">{{item.author.loginname}}</span>
+                <span class="status" v-if="item.reply_count > 0">
+                  <b>{{item.reply_count}}</b>/{{item.visit_count}}
+                </span>
+              </p>
+              <p>
+                <time>{{item.create_at}}</time>
+                <time>{{item.last_reply_at}}</time>
+              </p>
+            </div>
+          </div>
       </li>
     </ul>
   </section>
 </template>
 <script>
+  import $ from 'webpack-zepto';
+  import '@/assets/stylesheets/app.scss';
+
   export default {
     data() {
       return {
+        scroll: true,
         topics: [],
+        index: {},
         searchKey: {
-          page: 1
+          page: 1,
+          limit: 20,
+          tab: 'all',
+          mdrender: true
+        },
+        searchDataStr: ''
+      }
+    },
+    mounted() {
+      this.getTopics();
+    },
+    methods: {
+      getTopics() {
+        let params = $.param(this.searchKey);
+        console.log(params);
+        this.$http.get('https://cnodejs.org/api/v1/topics?' + params).then(response => {
+          this.scroll = true;
+          console.log(response.data.data);
+          if (response && response.data) {
+            response.data.data.forEach(this.mergeTopics);
+          }
+        }).catch(response => {
+          console.log(response);
+        });
+      },
+      mergeTopics(topic) {
+        if (typeof this.index[topic.id] === 'number') {
+          const topicsIndex = this.index[topic.id];
+          this.topics[topicsIndex] = topic;
+        } else {
+          this.index[topic.id] = this.topics.length;
+          this.topics.push(topic);
         }
       }
     }
